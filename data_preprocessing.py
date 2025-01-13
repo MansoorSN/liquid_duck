89b47d6a5d83542ee555dd78bdf2cf56c7c1d9fc
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2025-01-13 02:13:09 -0500

    refactored variable names

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,72 +1,68 @@
-#%%
+# %%
 import pandas as pd
 import duckdb as db
 
-con=db.connect(database='liquid_duck.db', read_only=False)
+# Connect to the DuckDB database
+connection = db.connect(database='liquid_duck.db', read_only=False)
 
 # %%
-#Create Products, Customers, Sales tables in duck db from the CSV files. 
-
-con.execute('''CREATE OR REPLACE TABLE PRODUCTS AS
-            Select
-            "Product_id"::INTEGER           AS Product_id,
-            "Supplier"::VARCHAR(255)        AS Supplier,
-            "Brand"::VARCHAR(255)           AS Brand,
-            "Family"::VARCHAR(255)          AS Family,
-            "Product_name"::VARCHAR(255)    AS Product_name,
-            "Product_cost"::DECIMAL(10,2)   AS Product_cost,
-            "Inventory_volume"::INTEGER     AS Inventory_volume
-
-            FROM read_csv_auto('datasets/products.csv', header=True); 
-            ''')
-
-
-# %%
-con.execute('''
-            CREATE OR REPLACE TABLE CUSTOMERS AS
-            SELECT 
-                "Customer_id"::INTEGER              AS Customer_id 
-            , "Customer_name"::VARCHAR(255)         AS Customer_name
-            , "Customer_address"::VARCHAR(255)      AS Customer_address
-            , "Customer_phone"::VARCHAR(255)        AS Customer_phone
-
-            FROM read_csv_auto('datasets/customers.csv', header=True);
-            ''')
-# %%
-
-con.execute('''
-            CREATE OR REPLACE TABLE SALES AS
-            SELECT
-            "sale_id"::INTEGER              AS Sale_id
-            , "Product_id"::INTEGER           AS Product_id
-            , "Customer_id"::INTEGER          AS Customer_id
-            , "Sale_date"::DATE               AS Sale_date
-            , "Sale_volume"::INTEGER          AS Sale_volume
-            , "Sale_revenue"::DECIMAL(10,2)   AS Sale_revenue
-            FROM read_csv_auto('datasets/sales.csv', header=True);
+# Create PRODUCTS table from the CSV file
+connection.execute('''
+    CREATE OR REPLACE TABLE products AS
+    SELECT
+        "Product_id"::INTEGER AS product_id,
+        "Supplier"::VARCHAR(255) AS supplier,
+        "Brand"::VARCHAR(255) AS brand,
+        "Family"::VARCHAR(255) AS family,
+        "Product_name"::VARCHAR(255) AS product_name,
+        "Product_cost"::DECIMAL(10,2) AS product_cost,
+        "Inventory_volume"::INTEGER AS inventory_volume
+    FROM read_csv_auto('datasets/products.csv', header=True);
 ''')
 
+# %%
+# Create CUSTOMERS table from the CSV file
+connection.execute('''
+    CREATE OR REPLACE TABLE customers AS
+    SELECT
+        "Customer_id"::INTEGER AS customer_id,
+        "Customer_name"::VARCHAR(255) AS customer_name,
+        "Customer_address"::VARCHAR(255) AS customer_address,
+        "Customer_phone"::VARCHAR(255) AS customer_phone
+    FROM read_csv_auto('datasets/customers.csv', header=True);
+''')
 
 # %%
-con.execute('''
-            CREATE OR REPLACE TABLE DENORMALIZED_SALES AS
-            SELECT * FROM
-           SALES S LEFT OUTER JOIN CUSTOMERS C
-            ON S.Customer_id=C.Customer_id 
-            FULL OUTER JOIN PRODUCTS P 
-            ON S.Product_id=P.Product_id   
-            ''')
+# Create SALES table from the CSV file
+connection.execute('''
+    CREATE OR REPLACE TABLE sales AS
+    SELECT
+        "sale_id"::INTEGER AS sale_id,
+        "Product_id"::INTEGER AS product_id,
+        "Customer_id"::INTEGER AS customer_id,
+        "Sale_date"::DATE AS sale_date,
+        "Sale_volume"::INTEGER AS sale_volume,
+        "Sale_revenue"::DECIMAL(10,2) AS sale_revenue
+    FROM read_csv_auto('datasets/sales.csv', header=True);
+''')
 
 # %%
-con.execute("COPY (FROM DENORMALIZED_SALES) TO 'datasets/denormalized_sales.csv' (HEADER, DELIMITER ',')")
+# Create DENORMALIZED_SALES table by joining SALES, CUSTOMERS, and PRODUCTS tables
+connection.execute('''
+    CREATE OR REPLACE TABLE denormalized_sales AS
+    SELECT *
+    FROM sales s
+    LEFT OUTER JOIN customers c ON s.customer_id = c.customer_id
+    FULL OUTER JOIN products p ON s.product_id = p.product_id;
+''')
 
 # %%
+# Export the DENORMALIZED_SALES table to a CSV file
+connection.execute(
+    "COPY (FROM denormalized_sales) TO 'datasets/denormalized_sales.csv' (HEADER, DELIMITER ',')"
+)
 
-df=con.execute('''
-            SELECT * 
-            FROM DENORMALIZED_SALES   
-            
-            ''').df()
-
-print(df.columns)
-print(df.head(30))
+# %%
+# Close the DuckDB connection
+connection.close()
+# %%
